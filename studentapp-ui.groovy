653b374a7c55c3d8b7ea0f7f94d89f62f6b63f93
jarvis <chetansomkuwar@gmail.com>
2024-03-15 20:38:19 +0530

    --- Auto Git Commit ---

--- studentapp-ui.groovy
+++ studentapp-ui.groovy
@@ -7,5 +7,20 @@ pipeline {
                 echo 'Hello World'
             }
         }
+                stage('Build') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+                stage('Pull') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+                stage('Pull') {
+            steps {
+                echo 'Hello World'
+            }
+        }
     }
 }
